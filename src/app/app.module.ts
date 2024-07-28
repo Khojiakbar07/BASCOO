c8714e4a864c3a3f699b725c1c441afe0476e662
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-07-28 14:55:38 +0500

    Added main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,42 @@
-import { APP_INITIALIZER, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './modules/navbar/navbar.component';
-import { FooterComponent } from './modules/footer/footer.component';
-import { LoginComponent } from './modules/auth/login/login.component';
-import { RegisterComponent } from './modules/auth/register/register.component';
-import { HomeComponent } from './modules/layouts/home/home.component';
-import { SharedModule } from 'src/shared/shared.module';
+import {APP_INITIALIZER, NO_ERRORS_SCHEMA, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './modules/navbar/navbar.component';
+import {FooterComponent} from './modules/footer/footer.component';
+import {LoginComponent} from './modules/auth/login/login.component';
+import {RegisterComponent} from './modules/auth/register/register.component';
+import {HomeComponent} from './modules/layouts/home/home.component';
+import {SharedModule} from 'src/shared/shared.module';
 import {NgOptimizedImage, registerLocaleData} from '@angular/common';
 import localeRu from "@angular/common/locales/ru";
-import { NZ_I18N, ru_RU , en_US } from 'ng-zorro-antd/i18n';
-import { AppInitService } from './core/local-services/appInitService/app-init-service.service';
-import { LanguageComponent } from './core/language/language.component';
-import { FindLanguageFromKeyPipe } from './core/language/find-language-from-key.pipe';
-import { CaruselComponent } from 'src/shared/components/home/carusel/carusel.component';
-import { HowIsItWorkComponent } from '../shared/components/home/how-is-it-work/how-is-it-work.component';
-import { PricesComponent } from '../shared/components/home/prices/prices.component';
-import { StoresComponent } from '../shared/components/home/stores/stores.component';
-import { ChooseUsComponent } from '../shared/components/home/choose-us/choose-us.component';
-import { ReviewsComponent } from '../shared/components/home/reviews/reviews.component';
-import { FaqComponent } from '../shared/components/home/faq/faq.component';
-import { AboutUsComponent } from './modules/layouts/about-us/about-us.component';
-import { ContactsComponent } from './modules/layouts/contacts/contacts.component';
-import { PriceComponent } from './modules/layouts/price/price.component';
-import { ServicesComponent } from './modules/layouts/services/services.component';
-import { HelpsComponent } from './modules/layouts/helps/helps.component';
-import { RestrictionsComponent } from 'src/shared/components/restrictions/restrictions.component';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MapComponent } from '../shared/components/home/map/map.component';
-import { AngularYandexMapsModule } from 'angular8-yandex-maps';
+import {NZ_I18N, ru_RU, en_US} from 'ng-zorro-antd/i18n';
+import {AppInitService} from './core/local-services/appInitService/app-init-service.service';
+import {LanguageComponent} from './core/language/language.component';
+import {FindLanguageFromKeyPipe} from './core/language/find-language-from-key.pipe';
+import {CaruselComponent} from 'src/shared/components/home/carusel/carusel.component';
+import {HowIsItWorkComponent} from '../shared/components/home/how-is-it-work/how-is-it-work.component';
+import {PricesComponent} from '../shared/components/home/prices/prices.component';
+import {StoresComponent} from '../shared/components/home/stores/stores.component';
+import {ChooseUsComponent} from '../shared/components/home/choose-us/choose-us.component';
+import {ReviewsComponent} from '../shared/components/home/reviews/reviews.component';
+import {FaqComponent} from '../shared/components/home/faq/faq.component';
+import {AboutUsComponent} from './modules/layouts/about-us/about-us.component';
+import {ContactsComponent} from './modules/layouts/contacts/contacts.component';
+import {PriceComponent} from './modules/layouts/price/price.component';
+import {ServicesComponent} from './modules/layouts/services/services.component';
+import {HelpsComponent} from './modules/layouts/helps/helps.component';
+import {RestrictionsComponent} from 'src/shared/components/restrictions/restrictions.component';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MapComponent} from '../shared/components/home/map/map.component';
+import {AngularYandexMapsModule} from 'angular8-yandex-maps';
 import {StatsComponent} from "./modules/layouts/stats/stats.component";
 import {AboutUsSectionComponent} from "../shared/components/home/about-us/about-us.component";
+import {ContactComponent} from "../shared/components/contacts/contacts.component"
 
 
 registerLocaleData(localeRu);
+
 export function initializeApp(appInitService: AppInitService) {
   return (): Promise<any> => {
     return new Promise((resolve, reject) => {
@@ -45,32 +47,33 @@ export function initializeApp(appInitService: AppInitService) {
 }
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavbarComponent,
-        FooterComponent,
-        LoginComponent,
-        RegisterComponent,
-        HomeComponent,
-        LanguageComponent,
-        FindLanguageFromKeyPipe,
-        CaruselComponent,
-        HowIsItWorkComponent,
-        StoresComponent,
-        ChooseUsComponent,
-        ReviewsComponent,
-        FaqComponent,
-        AboutUsComponent,
-        PricesComponent,
-        ContactsComponent,
-        PriceComponent,
-        ServicesComponent,
-        HelpsComponent,
-        RestrictionsComponent,
-        MapComponent,
-        StatsComponent,
-        AboutUsSectionComponent,
-    ],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    LanguageComponent,
+    FindLanguageFromKeyPipe,
+    CaruselComponent,
+    HowIsItWorkComponent,
+    StoresComponent,
+    ChooseUsComponent,
+    ReviewsComponent,
+    FaqComponent,
+    AboutUsComponent,
+    PricesComponent,
+    ContactsComponent,
+    PriceComponent,
+    ServicesComponent,
+    HelpsComponent,
+    RestrictionsComponent,
+    MapComponent,
+    StatsComponent,
+    AboutUsSectionComponent,
+    ContactComponent
+  ],
   imports: [
     BrowserModule,
     SharedModule,
@@ -85,7 +88,7 @@ export function initializeApp(appInitService: AppInitService) {
       provide: LocationStrategy,
       useClass: HashLocationStrategy
     },
-    { provide: NZ_I18N, useValue: ru_RU },
+    {provide: NZ_I18N, useValue: ru_RU},
     {
       provide: APP_INITIALIZER,
       useFactory: initializeApp,
@@ -95,4 +98,5 @@ export function initializeApp(appInitService: AppInitService) {
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
