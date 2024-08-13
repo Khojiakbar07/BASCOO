586d3124327fcdac28b28fe7166e3f764e4864c3
Khojiakbar07 <hojiakbar312@gmail.com>
2024-08-13 22:11:25 +0500

    Deleted Unuseless components

--- src/shared/components/user-page/user.module.ts
+++ src/shared/components/user-page/user.module.ts
@@ -5,15 +5,11 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { FormsModule } from '@angular/forms';
 import { LayoutsComponent } from 'src/shared/components/user-page/layouts/layouts.component';
-import { MyParcelsComponent } from './components2/my-parcels/my-parcels.component';
 import { MyAddressesComponent } from './components2/my-addresses/my-addresses.component';
 import { MyShipmentsComponent } from './components2/my-shipments/my-shipments.component';
 import { SettingsComponent } from './components2/settings/settings.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PasswordComponent } from './components2/password/password.component';
 import { PersonalInfoComponent } from './components2/personal-info/personal-info.component';
-import { MySendersComponent } from './components2/my-senders/my-senders.component';
-import { MyAcceptancesComponent } from './components2/my-acceptances/my-acceptances.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
@@ -24,14 +20,10 @@ export function HttpLoaderFactory(http: HttpClient) {
 @NgModule({
   declarations: [
     LayoutsComponent,
-    MyParcelsComponent,
     MyAddressesComponent,
     MyShipmentsComponent,
     SettingsComponent,
-    PasswordComponent,
     PersonalInfoComponent,
-    MySendersComponent,
-    MyAcceptancesComponent
   ],
   imports: [
     CommonModule,
