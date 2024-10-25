96cd25158130399e6a966ce632e31dce79b8fe74
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-10-25 11:25:27 +0500

    micro chagnes

--- src/shared/components/home/carusel/carusel.component.ts
+++ src/shared/components/home/carusel/carusel.component.ts
@@ -2,18 +2,17 @@ import { Component } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
-  selector: 'app-carusel',
-  templateUrl: './carusel.component.html',
-  styleUrls: ['./carusel.component.scss']
+	selector: 'app-carusel',
+	templateUrl: './carusel.component.html',
+	styleUrls: ['./carusel.component.scss'],
 })
 export class CaruselComponent {
-
-  array= [
-    "../../../../assets/images/carusel/DSC.jpg",
-    "../../../../assets/images/carusel/LSBB.jpg",
-    "../../../../assets/images/carusel/Office.png",
-    "../../../../assets/images/carusel/LED-Display2.webp",
-    "../../../../assets/images/carusel/ITC.jpg",
-    "../../../../assets/images/carusel/ITC1.jpg",
-  ]
+	array = [
+		'../../../../assets/images/carusel/DSC.jpg',
+		'../../../../assets/images/carusel/LSBB.jpg',
+		'../../../../assets/images/carusel/Office.png',
+		'../../../../assets/images/carusel/LED-Display2.webp',
+		'../../../../assets/images/carusel/conference-room.webp',
+		'../../../../assets/images/carusel/led-display.webp',
+	]
 }
